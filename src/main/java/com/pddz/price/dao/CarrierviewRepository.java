f853c30b70ef16b40ab01123fb479b41402b2c69
onesimu <872634214@qq.com>
2018-08-15 10:07:51 +0800

    添加公司和港口视图

---
+++ src/main/java/com/pddz/price/dao/CarrierviewRepository.java
@@ -0,0 +1,7 @@
+package com.pddz.price.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pddz.price.entity.Carrierview;
+
+public interface CarrierviewRepository extends JpaRepository<Carrierview, Integer> {
+}
