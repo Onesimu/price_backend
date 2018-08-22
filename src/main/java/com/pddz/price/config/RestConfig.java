e3722f91cebde8f3979d1cd56de6073da9f798df
onesimu <872634214@qq.com>
2018-08-22 19:57:54 +0800

    仍然设置暴露主键
    因前端字段已写好

---
+++ src/main/java/com/pddz/price/config/RestConfig.java
@@ -0,0 +1,22 @@
+package com.pddz.price.config;
+
+import com.pddz.price.entity.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return new RepositoryRestConfigurerAdapter() {
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+                config.exposeIdsFor(Carrier.class, Country.class, Port.class, Seaexpressprice.class, Userinfo.class);
+            }
+        };
+    }
+}
